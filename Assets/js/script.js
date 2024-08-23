60c0cc5151594bf1deadf8b5550fcc8a0e8c7e24
Ali Najafi <147813870+alinajafi-developer@users.noreply.github.com>
2024-08-23 21:02:23 +0330

    Add files via upload

---
+++ Assets/js/script.js
@@ -0,0 +1,9 @@
+document.querySelector('#main').addEventListener('click',function(){
+    window.location.href = "https://www.irantop5.com/";
+})
+
+function myFunc(){
+    document.getElementById("pointer1").classList.toggle("hidden")
+}
+
+setInterval(myFunc,500)
